a93e0cbd279ce4f41ca7df0695c00a4d7168704a
MichaelBamba <mjbamba2@gmail.com>
2020-01-21 11:40:21 -0500

    add friend append menthod

--- personclass.py
+++ personclass.py
@@ -1,19 +1,22 @@
 class Person:
-    def __init__(self, name, email, phone):
+    def __init__(self, name, email, phone, friends):
          self.name = name
          self.email = email
          self.phone = phone
+         self.friends = friends
 
     def greet(self, other_person):
          return('Hello {}, I am {}!'.format(other_person.name, self.name))
     def print_contact_info(self):
         return "{}'s email: {}, {}'s phone number: {}".format(self.name, self.email, self.name, self.phone)
-    def __init__ (friends):
-        self.name
 
 
-sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
-jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
+
+sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948", [])
+jordan = Person("Jordan", "jordan@aol.com", "495-586-3456", [])
+
+jordan.friends.append("sonny")
+sonny.friends.append("jordan")
 
 # print(sonny.greet(jordan))
 # print (jordan.greet(sonny))
@@ -25,4 +28,7 @@ jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
 # sonnycont = sonny.email, sonny.phone
 # print(sonnycont)
 # print (sonny.phone, sonny.email)
-print(sonny.print_contact_info())
+
+# print(sonny.print_contact_info())
+
+print(sonny.friends)
